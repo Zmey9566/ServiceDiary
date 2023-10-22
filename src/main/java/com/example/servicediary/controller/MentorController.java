8193f8f1c1d7f9a98cb5597b1e34d3f165dc16e0
Alex Zmey <top_secre99@mail.ru>
2023-10-22 11:26:15 +0300

    add Controllers

--- src/main/java/com/example/servicediary/controller/MentorController.java
+++ src/main/java/com/example/servicediary/controller/MentorController.java
@@ -1,15 +1,37 @@
 package com.example.servicediary.controller;
 
+import com.example.servicediary.dao.MentorDaoImpl;
+import com.example.servicediary.entity.Mentor;
+import com.example.servicediary.util.HibernateUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
-    @GetMapping()
-    public String index(){
-        return "/mentorIndex";
+    private final MentorDaoImpl mentorDao = new MentorDaoImpl(Mentor.class, HibernateUtil.buildSessionFactory()
+            .openSession());
+
+    @GetMapping("")
+    public String mentorIndex(Model model){
+        model.addAttribute("mentors", mentorDao.getAll());
+        return "mentor/mentorIndex";
+    }
+
+    @GetMapping("/{id}")
+    public String showMentor(@PathVariable("id")int id, Model model){
+        Mentor mentor = mentorDao.findById(id);
+        model.addAttribute("mentor", mentor);
+        return "mentor/showOne";
+    }
+
+    @GetMapping("/save")
+    public String saveStudent() {
+        return "mentor/save";
     }
 }
--- src/main/java/com/example/servicediary/controller/StudentController.java
+++ src/main/java/com/example/servicediary/controller/StudentController.java
@@ -1,11 +1,13 @@
 package com.example.servicediary.controller;
 
 import com.example.servicediary.dao.StudentDaoImpl;
+import com.example.servicediary.entity.Mentor;
 import com.example.servicediary.entity.Student;
 import com.example.servicediary.util.HibernateUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,7 +19,14 @@ public class StudentController {
     @GetMapping("")
     public String studentIndex(Model model){
         model.addAttribute("students", studentDao.getAll());
-        return "/studentIndex";
+        return "student/studentIndex";
+    }
+
+    @GetMapping("/{id}")
+    public String showStudent(@PathVariable("id")int id, Model model){
+        Student student = studentDao.findById(id);
+        model.addAttribute("student", student);
+        return "student/showOne";
     }
 
 }
