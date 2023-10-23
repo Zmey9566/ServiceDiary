9ddef0c25fc9c11ee290620083816be7eb814e2f
Alex Zmey <top_secre99@mail.ru>
2023-10-23 19:37:38 +0300

    add delete mentor controller

--- src/main/java/com/example/servicediary/controller/MentorController.java
+++ src/main/java/com/example/servicediary/controller/MentorController.java
@@ -5,10 +5,7 @@ import com.example.servicediary.entity.Mentor;
 import com.example.servicediary.util.HibernateUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/mentor")
@@ -17,21 +14,51 @@ public class MentorController {
     private final MentorDaoImpl mentorDao = new MentorDaoImpl(Mentor.class, HibernateUtil.buildSessionFactory()
             .openSession());
 
-    @GetMapping("")
+    @GetMapping()
     public String mentorIndex(Model model){
         model.addAttribute("mentors", mentorDao.getAll());
         return "mentor/mentorIndex";
     }
 
     @GetMapping("/{id}")
-    public String showMentor(@PathVariable("id")int id, Model model){
+    public String showMentor(@PathVariable("id")int id, Model model) {
         Mentor mentor = mentorDao.findById(id);
         model.addAttribute("mentor", mentor);
         return "mentor/showOne";
     }
 
     @GetMapping("/save")
-    public String saveStudent() {
+    public String saveStudent(@ModelAttribute("mentor") Mentor mentor) {
         return "mentor/save";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("mentor") Mentor mentor) {
+        mentorDao.save(mentor);
+        return "redirect:/mentor";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("mentor", mentorDao.findById(id));
+        return "mentor/edit";
+    }
+
+    @PostMapping("/{id}")
+    public String update(@ModelAttribute("mentor") Mentor mentor) {
+        mentorDao.update(mentor);
+        return "redirect:/mentor";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(Model model, @PathVariable("id") int id) {
+        model.addAttribute("mentor", mentorDao.findById(id));
+        return "mentor/delete";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deleteById(@ModelAttribute("mentor") Mentor mentor) {
+        mentorDao.removeById(mentor.getId());
+        return "redirect:/mentor";
     }
 }
