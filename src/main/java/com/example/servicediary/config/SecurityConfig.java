fc02bdd4340f9829b1df5092a8202b62d67db588
Zmey9566 <118925585+Zmey9566@users.noreply.github.com>
2023-12-17 10:30:00 +0300

    Add Role entity. Add columns email, role, password for Mentor. Update MentorServiceImpl for NoRest. Update tests. Update web. Add spring Security.

--- src/main/java/com/example/servicediary/config/SecurityConfig.java
+++ src/main/java/com/example/servicediary/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.servicediary.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +14,18 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
     }
 
 }
